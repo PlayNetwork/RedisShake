4b20c6a28f7911f03f21f2fd444f68e6b0804597
vpotra <vpotra@theoctavegroup.com>
2021-09-23 13:30:38 -0400

    Sync work from PN

---
+++ run.sh
@@ -0,0 +1,1 @@
+aws --profile=stage cloudformation create-stack --stack-name val-test-shake --parameters ParameterKey=JenkinsPassword,ParameterValue=${jpass} ParameterKey=JenkinsUser,ParameterValue=jenkins --template-body file:///Users/vpotra/work/git/RedisShake/cf.json
